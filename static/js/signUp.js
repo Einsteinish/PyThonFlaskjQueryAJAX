b803d5b264774b7c242c5b299e3c992337705075
K Hong <k.hong@aol.com>
2016-12-19 09:48:33 +0900

    initial commit

---
+++ static/js/signUp.js
@@ -0,0 +1,17 @@
+$(function(){
+	$('button').click(function(){
+		var user = $('#inputUsername').val();
+		var pass = $('#inputPassword').val();
+		$.ajax({
+			url: '/signUpUser',
+			data: $('form').serialize(),
+			type: 'POST',
+			success: function(response){
+				console.log(response);
+			},
+			error: function(error){
+				console.log(error);
+			}
+		});
+	});
+});
